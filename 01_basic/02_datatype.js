d753f15fde24e1cdad11ab24780010ac95a5bcd9
Priyanshu Maurya <satyasaadhika.aham@gmail.com>
2024-04-09 13:12:33 +0530

    Add datatype

---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,34 @@
+"use strict"; //treat all js code as newer version
+
+/*Datatype
+1 number => 2 to power 53
+2 bigint 
+3 string => ""
+4 boolean => true/flase
+5 null => standalone value (empty)
+6 undefined =>  
+7 symbol => unique 
+8 object 
+*/
+
+console.log(typeof undefined);// undefined
+console.log(typeof null);// object
+
+
+// Numbers:
+let length = 16;
+let weight = 7.5;
+
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+
+// Booleans
+let x = true;
+let y = false;
+
+// Object:
+const person = {firstName:"priyanshu", lastName:"Maurya"};
+
+//undefined
+let number;       // Now number is undefined
