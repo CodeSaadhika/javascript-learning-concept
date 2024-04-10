65562ba04cd630f181682bf09d36c825f618f627
Priyanshu Maurya <106907193+satyasaadhika@users.noreply.github.com>
2024-04-10 16:11:42 +0000

    add operations

--- 01_basic/03_conversionoperation.js
+++ 01_basic/03_conversionoperation.js
@@ -25,5 +25,27 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);//33
-console.log(typeof stringNumber); //string
+//console.log(stringNumber);//33
+//console.log(typeof stringNumber); //string
+
+// ******************************** Operations ****************************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+/*console.log(2+2); //4
+console.log(7-2);//5
+console.log(9*2);//18
+console.log(4**3);//64
+console.log(3/2);//1.5
+console.log(4%5);//4  */
+
+let str1 ="Hello"
+let str2 = " Everyone"
+
+let str3 = str1 + str2 ;
+
+console.log(str3)
+
+
