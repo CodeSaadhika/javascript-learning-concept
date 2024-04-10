5130ee8d1b27c3913e5e1287c20d2c2792c23856
Priyanshu Maurya <106907193+satyasaadhika@users.noreply.github.com>
2024-04-10 14:59:54 +0000

    conversion of datatype

---
+++ 01_basic/03_conversionoperation.js
@@ -0,0 +1,29 @@
+// conversion operation
+let score = "80"
+
+//console.log(typeof score);
+
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "80" => 80
+// "80abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+//1 => true; 0 => false
+//"" => false
+// "priyanshu" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);//33
+console.log(typeof stringNumber); //string
